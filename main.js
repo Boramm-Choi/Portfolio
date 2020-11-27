099165c6d80ff3a4804285cc1e7c10ebeed6d200
Boramm-Choi <73491757+Boramm-Choi@users.noreply.github.com>
2020-11-27 18:27:20 -0500

    JS: arrow up icon added

--- main.js
+++ main.js
@@ -39,6 +39,23 @@ home.style.opacity= 1- window.scrollY / homeHeight;
 
 
 
+// show "arrow-up button"
+const arrowUp= document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+if(window.scrollY>homeHeight /2){
+arrowUp.classList.add('visible');
+}else {
+arrowUp.classList.remove('visible');
+}
+});
+
+
+
+// handle click on the arrow up button
+
+arrowUp.addEventListener('click', ()=> {
+scrollIntoView('#Home');
+});
 
 function scrollIntoView(selector){
 const scrollTo= document.querySelector(selector);
